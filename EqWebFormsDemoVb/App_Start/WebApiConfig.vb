8e6a975cbf70f3a1830d776c8835a4c82c2627bb
Anton Ivanenko <anton_ivanenko@outlook.com>
2020-02-14 14:00:26 +0200

    Add Visual Basic WebForms demo

---
+++ EqWebFormsDemoVb/App_Start/WebApiConfig.vb
@@ -0,0 +1,42 @@
+﻿Imports System.Collections.Generic
+Imports System.Web.Http
+Imports System.Web.Http.Controllers
+Imports System.Web.Http.Routing
+
+Imports Korzh.EasyQuery.Services
+
+Public Module WebApiConfig
+    Public Sub Register(ByVal config As HttpConfiguration)
+
+        Korzh.EasyQuery.AspNet.License.Key = "w5T2jSxvpyT39qaz5N6wegGBP921ZI"
+        Korzh.EasyQuery.AspNet.JSLicense.Key = "AlzWbvUgrkISH9AEAEoV7wBKJXGX14"
+
+        '// Web API configuration and services
+        '// Web API routes
+        Dim customRouteProvider As New WebApiCustomDirectRouteProvider
+        config.MapHttpAttributeRoutes(customRouteProvider)
+
+        config.Routes.MapHttpRoute(
+            name:="DefaultApi",
+            routeTemplate:="api/{controller}/{id}",
+            defaults:=New With {.id = RouteParameter.Optional}
+        )
+
+        '// Register you exportes here
+        '// to make export works
+        EasyQueryManager.RegisterExporter("csv", New CsvDataExporter())
+        EasyQueryManager.RegisterExporter("excel-html", New ExcelHtmlDataExporter())
+
+    End Sub
+End Module
+
+Public Class WebApiCustomDirectRouteProvider
+    Inherits DefaultDirectRouteProvider
+    Protected Overrides Function GetActionRouteFactories(actionDescriptor As HttpActionDescriptor) As IReadOnlyList(Of IDirectRouteFactory)
+
+        '    '// inherit route attributes decorated on base class controller's actions
+        Return actionDescriptor.GetCustomAttributes(Of IDirectRouteFactory)(True)
+
+    End Function
+
+End Class
---
+++ EqWebFormsDemoVb/Controllers/AdvancedSearchController.vb
@@ -0,0 +1,36 @@
+﻿Imports System
+Imports System.Configuration
+Imports System.Data.SqlClient
+Imports System.Web.Http
+
+Imports Korzh.EasyQuery.AspNet
+Imports Korzh.EasyQuery.Services
+
+<RoutePrefix("api/easyquery")>
+Public Class AdvancedSearchController : Inherits EasyQueryApiController
+
+    Protected Overrides Sub ConfigureEasyQueryOptions(options As EasyQueryOptions)
+
+        options.UseManager(Of EasyQueryManagerSql)()
+        options.DefaultModelId = "CRMS"
+        options.BuildQueryOnSync = True
+
+        Dim connectionString As String = ConfigurationManager.ConnectionStrings.Item("DefaultConnection").ConnectionString
+        options.UseDbConnection(Of SqlConnection)(connectionString)
+
+        Dim path As String = System.Web.Hosting.HostingEnvironment.MapPath("~/App_Data")
+        Dim modelLoader As Func(Of IServiceProvider, IModelLoader) =
+                Function()
+                    Return New FileModelLoader(path)
+                End Function
+        Dim queryStore As Func(Of IServiceProvider, IQueryStore) =
+                Function()
+                    Return New FileQueryStore(path)
+                End Function
+        options.UseModelLoader(modelLoader)
+        options.UseQueryStore(queryStore)
+
+        options.UsePaging(30)
+
+    End Sub
+End Class
