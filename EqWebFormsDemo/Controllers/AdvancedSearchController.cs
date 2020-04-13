79bdb30c6860e5ff0d5e98f93d9cea0b429947d2
Anton Ivanenko <anton_ivanenko@outlook.com>
2020-04-13 14:31:44 +0300

    Set .NET version 5.3.6. Set JS version 6.0.7

--- EqWebFormsDemo/Controllers/AdvancedSearchController.cs
+++ EqWebFormsDemo/Controllers/AdvancedSearchController.cs
@@ -3,10 +3,22 @@ using System.Data.SqlClient;
 
 using Korzh.EasyQuery.Services;
 using Korzh.EasyQuery.AspNet;
-
+using System.Web;
 
 namespace EqWebFormsDemo.Controllers
 {
+
+    public class SessionPreExecuteTuner : IEasyQueryManagerTuner
+    {
+        public bool Tune(EasyQueryManager manager)
+        {
+            var districtCode = (string)HttpContext.Current.Session["DistrictCode"];
+
+            manager.Query.ExtraConditions.AddSimpleCondition("PregnantWoman.DistrictCode", "Equal", districtCode);
+
+            return true;
+        }
+    }
 
     [RoutePrefix("api/easyquery")]
     public class AdvancedSearchController : EasyQueryApiController
@@ -14,6 +26,7 @@ namespace EqWebFormsDemo.Controllers
 
         protected override void ConfigureEasyQueryOptions(EasyQueryOptions options)
         {
+         
             options.UseManager<EasyQueryManagerSql>();
             options.DefaultModelId = "NWindSQL";
             options.BuildQueryOnSync = true;
@@ -24,6 +37,8 @@ namespace EqWebFormsDemo.Controllers
             var path = System.Web.Hosting.HostingEnvironment.MapPath("~/App_Data");
             options.UseModelLoader((_) => new FileModelLoader(path));
             options.UseQueryStore((_) => new FileQueryStore(path));
+
+            options.AddPreExecuteTuner(new SessionPreExecuteTuner());
 
             options.UsePaging(30);
         }
